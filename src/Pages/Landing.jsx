e8d0840985d89d95a155b1ff2f5ef25f77d60dc7
Rushi23442 <waghrushi228@gmail.com>
2025-07-11 18:15:25 +0530

    removed unnessary imports

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
-import Footer from "../Components/Footer";
+
 import Hero from "../Components/Hero";
 
 const Landing = () => {
