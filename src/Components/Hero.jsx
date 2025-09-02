ab0ab60dd582a866cb76d5a64fd51e773403eb66
Rushi23442 <waghrushi228@gmail.com>
2025-09-02 22:21:39 +0530

    changed ui

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,18 +11,18 @@ const Hero = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white px-4">
-      <div className="mt-10  ml-15 mr-15 rounded-sm min-h-[300px] bg-gradient-to-br md:h-120 from-orange-600 via-green-600 to-teal-500 flex flex-col items-center justify-center gap-3 p-6">
-        <h2 className="font-bold text-xl text-gray-900 text-center">
+    <div className="min-h-screen bg-[#DDF4E7] px-4">
+      <div className="mt-10  ml-15 mr-15 rounded-sm min-h-[300px] bg-gradient-to-br md:h-120 from-orange-200 via-blue-200 to-teal-500 flex flex-col items-center justify-center gap-3 p-6">
+        <h2 className="font-bold text-2xl text-gray-600 text-center">
           Select your Mood
         </h2>
-        <p className="font-bold text-gray-900  text-center">
+        <p className="font-bold text-gray-600  text-center">
           Vibe Music according to mood with VibeStream
         </p>
-        <button className="rounded-full bg-blue-400 h-10 w-32 text-white">
+        <button className="rounded-full  bg-[#FEA405] h-10 w-32 text-gray-700 border-2 border-[#D96F32] hover:border-[#568F87] hover:shadow-lg hover:scale-105">
           {currentMood}
         </button>
-        <button className="rounded-full bg-blue-400 h-10 w-32 text-white" onClick={NavigateToPlaylist}>
+        <button className="rounded-full bg-[#FEA405]  h-10 w-32 text-gray-700 border-2 border-[#D96F32] hover:border-[#568F87] hover:shadow-lg hover:scale-105" onClick={NavigateToPlaylist}>
           Go to Playlist
         </button>
       </div>
@@ -31,7 +31,7 @@ const Hero = () => {
         {moods.map((mood, index) => (
           <button
             key={index}
-            className="rounded-xl ml-3 bg-blue-400 h-10 w-28 text-white hover:bg-blue-500 transition-colors"
+            className="rounded-xl ml-3  bg-[#FEA405] h-10 w-24 text-gray-700 border-2 border-[#D96F32] hover:bg-[#D96F32] transition-colors hover:border-[#568F87] hover:shadow-lg hover:scale-105"
             onClick={() => {
               setCurrentMood(mood);
             }}
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,18 +8,18 @@ const NavBar = () => {
    <div>
   <div
     id="nav"
-    className="flex flex-col md:flex-row items-center justify-between text-xl h-auto md:h-20 shadow-sm shadow-gray-500 px-4 py-3"
+    className="flex  bg-[#F7F7F7] flex-col md:flex-row items-center justify-between font-bold h-auto md:h-20 shadow-sm text-gray-500 shadow-gray-500 px-4 py-3"
   >
     <div id="left" className="mb-2 md:mb-0">
-      <Link to="/">VibeStream</Link>
+      <Link to="/" className='text-2xl'>VibeStream</Link>
     </div>
     <div
       id="right"
-      className="flex flex-col md:flex-row gap-4 md:gap-8 items-center"
+      className="flex flex-col md:flex-row gap-4 md:gap-8 items-center mr-40 "
     >
-      <Link to="/">Home</Link>
-      <Link to="/playlist">Playlist</Link>
-      <Link to="/fav">See Favourites</Link>
+      <Link to="/" className='p-6'>Home</Link>
+      <Link to="/playlist" className='p-6'>Playlist</Link>
+      <Link to="/fav" className='p-6'> Favourites</Link>
     </div>
   </div>
 </div>
